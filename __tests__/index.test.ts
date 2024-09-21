4f998bf7a0ad7b3dbbd0d276e1d5835e58e45a31
simone <simonegauli@gmail.com>
2024-09-21 13:40:55 +0200

    improve test suite

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -2,16 +2,18 @@ import { describe, it } from '@jest/globals';
 import path from 'node:path';
 import ffmpeg from '../src/ffmpeg.js';
 import fs from 'node:fs';
+import coraline, { temporaryDirectorySync } from 'coraline';
 
 const input = path.join('media', 'video.mp4');
-const output = 'example.mp4';
-const outputStream = 'example_stream.mp4';
+const temporaryDir = temporaryDirectorySync();
+const output = path.join(temporaryDir, 'output.mp4');
+const outputStream = path.join(temporaryDir, 'output-stream.mp4');
 
 describe('concat function', () => {
   it(
     'should convert a stream with ffmpeg successfully',
     async () => {
-      await ffmpeg({ debug: true, input: fs.createReadStream(input), output: path.join('media', outputStream) });
+      await ffmpeg({ debug: true, input: fs.createReadStream(input), output: outputStream });
     },
     2 * 60 * 1000,
   );
@@ -19,8 +21,12 @@ describe('concat function', () => {
   it(
     'should convert a File with ffmpeg successfully',
     async () => {
-      await ffmpeg({ input, output: path.join('media', output) });
+      await ffmpeg({ input, output });
     },
     2 * 60 * 1000,
   );
+
+  afterAll(async () => {
+    await coraline.rm(temporaryDir);
+  });
 });
