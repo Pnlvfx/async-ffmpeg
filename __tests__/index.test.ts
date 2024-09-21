3771267d3a81dbaa794db53bed612cf9da7d4c46
simone <simonegauli@gmail.com>
2024-09-21 12:53:29 +0200

    improve test

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,27 +1,25 @@
-/* eslint-disable sonarjs/no-commented-code */
 import { describe, it } from '@jest/globals';
 import path from 'node:path';
 import ffmpeg from '../src/ffmpeg.js';
+import fs from 'node:fs';
 
-const getFile = (file: string) => path.join(process.cwd(), file);
+const input = path.join('media', 'video.mp4');
 const output = 'example.mp4';
-// const outputStream = 'example_stream.mp4';
+const outputStream = 'example_stream.mp4';
 
 describe('concat function', () => {
-  // it(
-  //   'should convert a stream with ffmpeg successfully',
-  //   async () => {
-  //     const file = getFile('media/video.mp4');
-  //     await ffmpeg({ debug: true, input: fs.createReadStream(file), output: path.join('media', outputStream) });
-  //   },
-  //   2 * 60 * 1000,
-  // );
+  it(
+    'should convert a stream with ffmpeg successfully',
+    async () => {
+      await ffmpeg({ debug: true, input: fs.createReadStream(input), output: path.join('media', outputStream) });
+    },
+    2 * 60 * 1000,
+  );
 
   it(
     'should convert a File with ffmpeg successfully',
     async () => {
-      const file = getFile('media/video.mp4');
-      await ffmpeg({ input: file, output: path.join('media', output) });
+      await ffmpeg({ input, output: path.join('media', output) });
     },
     2 * 60 * 1000,
   );
