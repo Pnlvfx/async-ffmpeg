eb1f727f67a303485568b99ecc5fb587c5e64a4d
simone <simonegauli@gmail.com>
2024-11-05 16:15:35 +0100

    fix time parse

--- src/ffmpeg.ts
+++ src/ffmpeg.ts
@@ -3,14 +3,14 @@ import { getParams } from './lib/params.js';
 import { startCommand } from './lib/process.js';
 import internal from 'node:stream';
 
-export async function ffmpeg({ debug, ...params }: FFmpegParams) {
+export const ffmpeg = async ({ debug, ...params }: FFmpegParams) => {
   const ffmpegParams = getParams(params);
   if (debug) {
     // eslint-disable-next-line no-console
     console.log(...ffmpegParams);
   }
   await startCommand('ffmpeg', ffmpegParams, params.input instanceof internal.Readable ? params.input : undefined);
-}
+};
 
 export { ffprobe } from './ffprobe.js';
 export { screenCapture } from './screen-capture.js';
--- src/lib/params.ts
+++ src/lib/params.ts
@@ -100,16 +100,26 @@ const parseInput = (input: string | string[] | internal.Readable) => {
 
 const parseTime = (value: Time | number) => {
   if (typeof value === 'number') return value.toString();
-  const { hours, milliseconds, minutes, seconds } = value;
+  const { hours = 0, milliseconds = 0, minutes = 0, seconds = 0 } = value;
+  const totalMilliseconds = (hours * 3600 + minutes * 60 + seconds) * 1000 + milliseconds;
+  const finalHours = Math.floor(totalMilliseconds / 3_600_000);
+  const finalMinutes = Math.floor((totalMilliseconds % 3_600_000) / 60_000);
+  const finalSeconds = Math.floor((totalMilliseconds % 60_000) / 1000);
+  const finalMilliseconds = totalMilliseconds % 1000;
+  return formatTime(finalHours, finalMinutes, finalSeconds, finalMilliseconds);
+};
+
+const formatTime = (hours: number, minutes: number, seconds: number, milliseconds: number) => {
   const fixedHours = formatTimeUnit(2, hours);
   const fixedMinutes = formatTimeUnit(2, minutes);
   const fixedSeconds = formatTimeUnit(2, seconds);
   const fixedMs = formatTimeUnit(3, milliseconds);
+
   return `${fixedHours}:${fixedMinutes}:${fixedSeconds}.${fixedMs}`;
 };
 
 const formatTimeUnit = (length: number, unit?: number) => {
-  if (unit) {
+  if (unit !== undefined) {
     const unitStr = unit.toString();
     return unitStr.length === length ? unitStr : '0'.repeat(length - unitStr.length) + unitStr;
   }
