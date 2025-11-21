8c7444e89091de62a93e1bb4ba65f0a846bcb2c0
Simone Gauli <simonegauli@gmail.com>
2025-11-21 22:29:59 +0100

    bump deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
-import { goateslint } from '@goatjs/node-eslint';
+import { nodeConfigs } from '@goatjs/node-eslint';
+import { defineConfig, globalIgnores } from '@eslint/config-helpers';
 
-export default goateslint({ ignores: ['dist', '.yarn'], tsconfigRootDir: import.meta.dirname });
+export default defineConfig([globalIgnores(['dist', '.yarn']), ...nodeConfigs({ tsconfigRootDir: import.meta.dirname })]);
